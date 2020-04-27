269e90da29110467bbb77be81aa896513faa2357
Jonathan <jonathanpalma89@gmail.com>
2020-04-27 19:12:59 +0100

    chore: add component with the parameter `props`

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import First from './components/First';
+import GoodMorning from './components/GoodMorning'
 
-ReactDOM.render(<First />, document.getElementById("root"))
+ReactDOM.render(<GoodMorning name="Jonathan" />, document.getElementById("root"))
