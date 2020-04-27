97fb5e2cb1682775b26bd9f1a073216fb5d453a8
Jonathan <jonathanpalma89@gmail.com>
2020-04-27 21:19:21 +0100

    refactor: use childrenWithProps function

--- src/components/Parent.jsx
+++ src/components/Parent.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import Child from './Child';
+import { childrenWithProps } from '../utils/utils'
 
 export default props => 
   <div>
     <h1>{props.name} {props.lastName}</h1>
     <h2>Childs</h2>
     <ul>
-      <Child name="Jonathan" lastName={props.lastName} />
-      <Child {...props} />
-      <Child {...props} name="Jean" />
+      {
+        childrenWithProps(props)
+      }
     </ul>
   </div>
--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Parent from './components/Parent';
+import Child from './components/Child';
 
 ReactDOM.render(
   <div>
-    <Parent name="Bob" lastName="Williams" />
+    <Parent name="Bob" lastName="Williams">
+      <Child name="Jonathan" />
+      <Child name="Jean" />
+      <Child name="Tiago" />
+    </Parent>
 </div>, 
 document.getElementById("root"))
